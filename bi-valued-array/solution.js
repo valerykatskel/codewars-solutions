064763efd71f3ca517bd6b9c2b7ba1d60902badc
Valery Katskel <ne.vse.srazu@gmail.com>
2020-01-10 10:24:52 +0300

    bi-valued array solution added

--- bi-valued-array/solution.js
+++ bi-valued-array/solution.js
@@ -1,35 +1,27 @@
-INPROGRESS
-
 function solution(A) {
-    let result = []
-  
+    let first = -1, second = -1, count = 0, maxLength = 0;
   
     A.forEach((el, index, ar) => {
-      if (result.length === 0) {
-          result.push({
-              value: el, firstPos: index, lastPos: 0, count: 1
-          })
+      if (first === -1) {
+        first = el
+        count ++
       } else {
-          let found = false
-          // need check if there is current number el or not in array
-          result.forEach(nmb => {
-              if (nmb.value === el) {
-                  nmb.count += 1
-                  nmb.lastPos = index
-                  found = true
-              }
-          })
-          
-          if (!found) {
-             result.push({
-                  value: el, firstPos: index, lastPos: 0, count: 1
-              })
+        if (second === -1) {
+          second = el  
+          count ++ 
+        } else {
+          if (el === first || el === second) {
+            count ++
+          } else {
+            if (count > maxLength) maxLength = count
+            count = 2
+            first = second
+            second = el
+            
           }
-      }        
+        }
+      }
     });
-    //if (result.length === 2) return A.length
-    //if (result.length === 1) return result[0].count
-    return result
+    if (count > maxLength) maxLength = count
+    return maxLength
   }
-  
-  console.log(solution([0, 5, 4, 4, 5, 12]));
