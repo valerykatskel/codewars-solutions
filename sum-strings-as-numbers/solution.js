3ce36d60da1953843a283f42ea4bb5bfb6e30888
Valery Katskel <ne.vse.srazu@gmail.com>
2020-01-10 10:28:11 +0300

    'sum strings as numbers' solution added

--- sum-strings-as-numbers/solution.js
+++ sum-strings-as-numbers/solution.js
@@ -1,38 +1,55 @@
-// IN PROGRESS
-
-function sumStrings(a,b) { 
-	if (!isNaN(a) && !isNaN(b) ) {
-	  let int1 = a==='' ? [0] : a.split('')
-	  let int2 = b==='' ? [0] : b.split('')
-	  let part1 = [], part2 = []
+function sumStrings(a, b) {
+	if (!isNaN(a) && !isNaN(b)) {
+	  let int1 = a === "" ? [0] : a.split("")
+	  let int2 = b === "" ? [0] : b.split("")
+	  let part1 = [],
+		part2 = [];
 	  if (a.length > b.length) {
 		part1 = int1
 		part2 = int2
-	  } else {
+	  } else if (a.length < b.length) {
 		part1 = int2
 		part2 = int1
+	  } else {
+		part1 = int1
+		part2 = int2
 	  }
-	  let result = ''
+	  let result = ""
 	  let add = 0
-	  let len1 = part1.length-1
-	  let len2 = part2.length-1
+	  let len1 = part1.length - 1
+	  let len2 = part2.length - 1
 	  let cSumArr = []
 	  for (let i = 0; i < part1.length; i++) {
-		let cSum = part1[len1-i] + part2[len2-i] + add
-		
-		if (cSum > 9) {
-		  // for example 18
-		  cSumArr = cSum.split('') // [1,8]
-		  add = cSum[0] // 1
-		  result += cSumArr[1] // 8
-		} else {
-		  // for example 9
-		  result += cSum // 9
-		  add = 0
+		let val1 = parseInt(part1[len1 - i], 10)
+		let val2 = len2 - i < 0 ? 0 : parseInt(part2[len2 - i], 10)
+		if (val1 > 0 || val2 > 0 || add > 0 || i < part1.length) {
+		  let valAdd = parseInt(add, 10)
+		  let cSum = val1 + val2 + valAdd
+		  if (cSum > 9) {
+			cSumArr = (cSum + "").split("")
+			let cSumVal = cSumArr[1]
+			add = cSumArr[0]
+			
+			if (i === part1.length - 1) {
+			  result += cSumArr[1] + cSumArr[0]
+			} else {
+			  result += cSumArr[1]
+			}
+		  } else {
+			add = 0
+			result += cSum
+		  }
 		}
 	  }
+  
 	  return result
+		.split("")
+		.reverse()
+		.filter((el, index, ar) => {
+		  return (index === 0 && el > 0) || index > 0 
+		})
+		.join("")
 	} else {
-	  return ''
+	  return ""
 	}
   }
