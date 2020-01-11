ea09da622a7063212ab40ef787e13ec39185f804
Valery Katskel <ne.vse.srazu@gmail.com>
2020-01-12 02:48:17 +0300

    solution 'Narcissic number' updated

--- narcissic-number/solution.js
+++ narcissic-number/solution.js
@@ -1,9 +1,6 @@
-// IN PROGRESS
-
 function narcissistic(value) {
 	const values = (''+value).split('')
 	const exponent = values.length
 	
 	return value === values.reduce((sum, current) => sum + Math.pow(parseInt(current, 10),exponent), 0)
-  }
-  
+}  
