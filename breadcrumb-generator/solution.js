c0ddd735805ba34b62909fbce1d52e919bd522c3
Valery Katskel <ne.vse.srazu@gmail.com>
2020-01-14 17:37:35 +0300

    Solution for 'breadcrumb generator' updated. Still in progress

--- breadcrumb-generator/solution.js
+++ breadcrumb-generator/solution.js
@@ -1,47 +1,57 @@
 function generateBC(url, separator) {
-	const delimiter = separator.trim()
-	let activeElementId = -1
-	const notIncludeIndexPages = (el, index, ar) => { 
-	  if (index === 0) {
-		return `<a href="/">HOME</a>`  
-	  } else {
-		let prevParst = ar.filter((el,idx) => idx > 0 && idx !== ar.length-1)
+	const delimiter = separator.trim();
+	let activeElementId = -1;
+	const makeHome = (el, index) => (index === 0 ? `<a href="/">HOME</a>` : el);
+	const trimAncors = el => (el.split("#").length > 1 ? el.split("#")[0] : el);
+	const makeLinks = (el, index, ar) => {
+	  //console.log(`index=${index} >> ar.length-1=${ar.length - 1} | el=${el}`);
+	  if (index > 0) {
+		let prevParts = ar.filter((el, idx) => idx > 0);
+		//console.log(prevParts);
+		const path = `${prevParts.slice(0, index).join("/")}/`
+		if (index !== ar.length - 1) {
+		  //console.log(path)
 		
-		if (index !== ar.length-1) {
-		  if (index > 1) {
-			return `<a href="/${prevParst.slice(0,index).join('/')}/">${el.toUpperCase()}</a>`
+		  if (index+1 <= ar.length-1 && ar[index+1].split(".")[0] === 'index') {
+			return `<span class="active">${
+			  el.toUpperCase().split(".")[0]
+			}</span>`;
 		  } else {
-			return `<a href="/${el}/">${el.toUpperCase()}</a>`
+			return `<a href="/${path}">${el.toUpperCase()}</a>`;
 		  }
 		} else {
-		  if (el.split('.')[0] !== 'index') {
-			return `<span class="active">${el.toUpperCase().split('.')[0]}</span>`
+		  //console.log(`the last | ${ar[index-1]}`)
+		  if (el.split(".")[0] !== 'index') {
+			return `<span class="active">${el.toUpperCase().split('.')[0]}</span>`;
 		  } else {
-			activeElementId = index-1
+			activeElementId = index - 1;
 		  }
-		} 
-	  }
-	}
-	
-	const removeUndefined = el => el !== undefined
-	
-	const getActiveElement = (el,id) => {
+		}
+	  } else return el;
+	};
+  
+	const removeUndefined = el => el !== undefined;
+  
+	const getActiveElement = (el, id) => {
 	  if (id !== activeElementId) {
-		return el
+		return el;
 	  } else {
-		const activeArr = ' class="active" '.split('')
-		let elArr = el.split('')
-		elArr.splice(2, 1, ...activeArr)
-		return elArr.join('')
+		const activeArr = ' class="active" '.split("");
+		let elArr = el.split("");
+		elArr[1] === 'a'? elArr.splice(2, 1, ...activeArr): elArr.splice(5, 1, ...activeArr);
+		
+		return elArr.join("");
 	  }
-	}
-	const addActiveClass = (el,id) => getActiveElement(el, id)
+	};
+	const addActiveClass = (el, id) => getActiveElement(el, id);
   
 	return url
-					.split('/')
-					.map(notIncludeIndexPages)
-					.map(addActiveClass)
-					.filter(removeUndefined)
-					.join(` ${delimiter} `)
+	  .split("/")
+	  .map(makeHome)
+	  .map(trimAncors)
+	  .map(makeLinks)
+	  //.map(addActiveClass)
+	  .filter(removeUndefined)
+	  .join(` ${delimiter} `);
 	//console.log(parts)
   }
