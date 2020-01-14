fa58a3bb97fc7ada8805ca49a31af4a71e06f413
Valery Katskel <ne.vse.srazu@gmail.com>
2020-01-14 12:16:02 +0300

    solution for 'Breadcrumb generator' updated

--- breadcrumb-generator/solution.js
+++ breadcrumb-generator/solution.js
@@ -1,29 +1,46 @@
 function generateBC(url, separator) {
 	const delimiter = separator.trim()
+	let activeElementId = -1
 	const notIncludeIndexPages = (el, index, ar) => { 
 	  if (index === 0) {
 		return `<a href="/">HOME</a>`  
 	  } else {
-		if (el.split('.')[0] !== 'index') {
-		  let prevParst = []
-		  if (index !== ar.length-1) {
-			if (index > 1) {
-			  prevParst = ar.filter((el,idx) => idx > 0 && idx !== ar.length-1)
-			  return `<a href="/${prevParst.join('/')}">${el.toUpperCase()}</a>`
-			} else {
-			  return `<a href="/${el}">${el.toUpperCase()}</a>`
-			}
+		let prevParst = ar.filter((el,idx) => idx > 0 && idx !== ar.length-1)
+		
+		if (index !== ar.length-1) {
+		  if (index > 1) {
+			return `<a href="/${prevParst.join('/')}/">${el.toUpperCase()}</a>`
 		  } else {
+			return `<a href="/${el}/">${el.toUpperCase()}</a>`
+		  }
+		} else {
+		  if (el.split('.')[0] !== 'index') {
 			return `<span class="active">${el.toUpperCase().split('.')[0]}</span>`
+		  } else {
+			activeElementId = index-1
 		  }
 		} 
 	  }
 	}
+	
 	const removeUndefined = el => el !== undefined
+	
+	const getActiveElement = (el,id) => {
+	  if (id !== activeElementId) {
+		return el
+	  } else {
+		const activeArr = ' class="active" '.split('')
+		let elArr = el.split('')
+		elArr.splice(2, 1, ...activeArr)
+		return elArr.join('')
+	  }
+	}
+	const addActiveClass = (el,id) => getActiveElement(el, id)
   
-	const parts = url
+	return url
 					.split('/')
 					.map(notIncludeIndexPages)
+					.map(addActiveClass)
 					.filter(removeUndefined)
 					.join(` ${delimiter} `)
 	//console.log(parts)
