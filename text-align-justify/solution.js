9621d2f9b15d32b0e69c306d10164f73a9886b13
Valery Katskel <ne.vse.srazu@gmail.com>
2020-01-17 16:29:04 +0300

    solution for 'Text align justify' updated. In progress

--- text-align-justify/solution.js
+++ text-align-justify/solution.js
@@ -1,37 +1,20 @@
 // IN PROGRESS
 function justify(str, len) {
 	let strAr = str.split(" ");
-  
 	const getGaps = num => [...Array(num).keys()].map(elem => " ").join("");
-	//console.log(getGaps(4));
 	const gapsWords = ar => {
-	  //console.log("---------");
-	  const wordsCount = ar.length;
 	  const totalLength = ar.reduce((sum, current) => sum + current.length, 0);
 	  let gapsLength = len - totalLength; //5
-	  const gapLength = Math.round(gapsLength / (wordsCount - 1));
-	  //console.log(gapLength);
-	  let sumGaps = 0;
 	  const addGaps = (el, index, ar) => {
-		const cnt = ar.length - index // 3
-		const currentGapSize = Math.round(gapsLength / cnt) // 3/3
-		gapsLength -= currentGapSize // 3-1=3
-		if (index < ar.length - 1) {
-		  //cost currentGap = gapsLength -
-		  return `${el}${getGaps(currentGapSize)}`;
-		} else {
-		  return `${el}`;
-		}
+		const cnt = ar.length - index
+		const currentGapSize = Math.round(gapsLength / cnt)
+		gapsLength -= currentGapSize
+		return (index < ar.length - 1) ? `${el}${getGaps(currentGapSize)}` : `${el}`
 	  };
-	  //const gapsLengthArr = Array(wordsForGap)
-	  //return gapsLengthArr.map(e => 0)
-	  //return Math.round(gapsLength / wordsCount)
 	  return ar.map(addGaps).join("");
 	};
-	//return gapsWords(["Lorem", "ipsum", "dolor"]);
 	let tStr = "";
 	let result = [];
-	
 	strAr.forEach((el, idx) => {
 	  if (len - tStr.length >= 0) {
 		if (len - tStr.length - 1 - el.length >= 0) {
@@ -44,4 +27,11 @@ function justify(str, len) {
 	});
 	return result.map(el => gapsWords(el.split(" "))).join("\n");
   }
+  
+  console.log(
+	justify(
+	  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quam ante, ultricies at eleifend interdum, bibendum id velit. Praesent sodales dolor eu aliquet pharetra. Fusce id tellus arcu. Suspendisse at ornare magna, nec interdum augue. Nullam eu cursus dui. Aliquam tincidunt in nibh eu bibendum. Phasellus scelerisque placerat quam at consectetur. Mauris cursus ipsum nec risus gravida volutpat. Mauris sapien dolor, lacinia id vehicula sit amet, gravida a enim. Ut congue nunc nec mattis pellentesque. Curabitur venenatis commodo nibh eget molestie.",
+	  30
+	)
+  );
   
